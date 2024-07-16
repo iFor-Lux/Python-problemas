78861ff6febb67a764addf3a30a20dd93876a66f
Luxury . dev <162370869+iFor-Lux@users.noreply.github.com>
2024-07-16 14:23:35 -0500

    Delete 4 directory

--- 4/problema3.py
+++
@@ -1,9 +0,0 @@
-#Crea un programa que pida al usuario que ingrese la distancia en kilometros y luego imprima la distancia en metros y en millas
-
-kilometros = int(input("Coloque la distancia en Kilometros: "))
-
-metros = kilometros * 1000
-
-millas = kilometros / 1,609
-
-print(f"La distancia en Metros es: {metros} y La distancia en Millas es: {millas}")
