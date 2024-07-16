4e1eed4da40aa3d8606ad9ab6b41eb33f867ab23
Luxury . dev <162370869+iFor-Lux@users.noreply.github.com>
2024-07-16 14:19:22 -0500

    Add files via upload

---
+++ 4/problema1.py
@@ -0,0 +1,9 @@
+#Crear un algoritmo que pida al usuario que ingrese 3 numeros y luego imprima el promedio de esos numeros
+
+nota1=int(input("Coloca el primer numero: "))
+nota2=int(input("Coloca el segundo numero: "))
+nota3=int(input("Coloca el tercer numero: "))
+
+promedio=(nota1 + nota2 + nota3) / 3
+
+print(f"El promedio es de: {promedio}")
---
+++ 4/problema5.py
@@ -0,0 +1,1 @@
+#Crea un programa que pida al usuario que ingrese un numero y luego imrprima si es un numero perfecto ( la suma de sus divisore, excepto el propio numero, es igual al propio numero )
